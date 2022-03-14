d22bccc3002ec17e60548a645b93b79ff3f95517
Harrison Marshall <harry130899@hotmail.com>
2022-03-14 12:50:34 +1100

    CPU: `use Instruction::*` in tests
    This removes a lot of unnecessary name qualification

--- components/eighty-eighty/src/tests.rs
+++ components/eighty-eighty/src/tests.rs
@@ -1,6 +1,7 @@
 use crate::cpu::{self, Cpu};
 use crate::instruction::Reg;
 use crate::Instruction;
+use Instruction::*;
 
 #[test]
 fn parity() {
@@ -51,12 +52,12 @@ fn mvi_and_add() -> Result<(), cpu::Error> {
 
     assert_eq!(cpu.a, 0);
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::B,
         value: 20,
     })?;
 
-    cpu.execute_instruction(Instruction::ADD {
+    cpu.execute_instruction(ADD {
         register: crate::instruction::Reg::B,
     })?;
 
@@ -70,7 +71,7 @@ fn mvi_and_add() -> Result<(), cpu::Error> {
 fn lxi() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x0055,
     })?;
@@ -78,7 +79,7 @@ fn lxi() -> Result<(), cpu::Error> {
     assert_eq!(cpu.b, 0x00);
     assert_eq!(cpu.c, 0x55);
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x1200,
     })?;
@@ -86,7 +87,7 @@ fn lxi() -> Result<(), cpu::Error> {
     assert_eq!(cpu.b, 0x12);
     assert_eq!(cpu.c, 0x00);
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::D,
         value: 0x1234,
     })?;
@@ -106,23 +107,23 @@ fn stax() -> Result<(), cpu::Error> {
 
     cpu.a = 0x34;
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0xabcd,
     })?;
 
-    cpu.execute_instruction(Instruction::STAX { register: Reg::B })?;
+    cpu.execute_instruction(STAX { register: Reg::B })?;
 
     assert_eq!(cpu.memory[0xabcd], 0x34);
 
     cpu.a = 0x12;
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::D,
         value: 0x7831,
     })?;
 
-    cpu.execute_instruction(Instruction::STAX { register: Reg::D })?;
+    cpu.execute_instruction(STAX { register: Reg::D })?;
 
     assert_eq!(cpu.memory[0xabcd], 0x34);
     assert_eq!(cpu.memory[0x7831], 0x12);
@@ -140,15 +141,14 @@ fn inx() -> Result<(), cpu::Error> {
     cpu.condition_codes.s = 14;
     cpu.condition_codes.cy = 16;
     cpu.condition_codes.ac = 17;
-    cpu.condition_codes.pad = 18;
 
     // Test for proper wrapping
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0xffff,
     })?;
 
-    cpu.execute_instruction(Instruction::INX { register: Reg::B })?;
+    cpu.execute_instruction(INX { register: Reg::B })?;
 
     assert_eq!(cpu.b, 0x00);
     assert_eq!(cpu.c, 0x00);
@@ -158,14 +158,13 @@ fn inx() -> Result<(), cpu::Error> {
     assert_eq!(cpu.condition_codes.s, 14);
     assert_eq!(cpu.condition_codes.cy, 16);
     assert_eq!(cpu.condition_codes.ac, 17);
-    assert_eq!(cpu.condition_codes.pad, 18);
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0xfffe,
     })?;
 
-    cpu.execute_instruction(Instruction::INX { register: Reg::B })?;
+    cpu.execute_instruction(INX { register: Reg::B })?;
 
     assert_eq!(cpu.b, 0xff);
     assert_eq!(cpu.c, 0xff);
@@ -184,12 +183,12 @@ fn inx() -> Result<(), cpu::Error> {
 fn inr_register() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::INR { register: Reg::A })?;
+    cpu.execute_instruction(INR { register: Reg::A })?;
 
     assert_eq!(cpu.a, 0x01);
     assert_eq!(cpu.condition_codes.cy, 0);
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 0xff,
     })?;
@@ -197,7 +196,7 @@ fn inr_register() -> Result<(), cpu::Error> {
     assert_eq!(cpu.a, 0xff);
     assert_eq!(cpu.condition_codes.cy, 0);
 
-    cpu.execute_instruction(Instruction::INR { register: Reg::A })?;
+    cpu.execute_instruction(INR { register: Reg::A })?;
 
     assert_eq!(cpu.a, 0x00);
     assert_eq!(cpu.condition_codes.cy, 0);
@@ -210,11 +209,11 @@ fn inr_register() -> Result<(), cpu::Error> {
 fn inr_memory() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::INR { register: Reg::M })?;
+    cpu.execute_instruction(INR { register: Reg::M })?;
 
     assert_eq!(cpu.memory[0], 1);
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::H,
         value: 0x1234,
     })?;
@@ -222,7 +221,7 @@ fn inr_memory() -> Result<(), cpu::Error> {
     assert_eq!(cpu.h, 0x12);
     assert_eq!(cpu.l, 0x34);
 
-    cpu.execute_instruction(Instruction::INR { register: Reg::M })?;
+    cpu.execute_instruction(INR { register: Reg::M })?;
 
     assert_eq!(cpu.memory[0x1234], 1);
 
@@ -234,14 +233,14 @@ fn inr_memory() -> Result<(), cpu::Error> {
 fn dcr_register() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::DCR { register: Reg::B })?;
+    cpu.execute_instruction(DCR { register: Reg::B })?;
 
     assert_eq!(cpu.b, 0xff);
     assert_eq!(cpu.condition_codes.cy, 0);
     assert_eq!(cpu.condition_codes.s, 1);
     assert_eq!(cpu.condition_codes.p, 1);
 
-    cpu.execute_instruction(Instruction::DCR { register: Reg::B })?;
+    cpu.execute_instruction(DCR { register: Reg::B })?;
 
     assert_eq!(cpu.b, 0xfe);
     assert_eq!(cpu.condition_codes.cy, 0);
@@ -274,7 +273,7 @@ fn dcr_memory() -> Result<(), cpu::Error> {
 fn mvi_reg() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::B,
         value: 0x12,
     })?;
@@ -289,12 +288,12 @@ fn mvi_reg() -> Result<(), cpu::Error> {
 fn mvi_mem() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::H,
         value: 0x1234,
     })?;
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::M,
         value: 0x12,
     })?;
@@ -309,7 +308,7 @@ fn mvi_mem() -> Result<(), cpu::Error> {
 fn rlc() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 0xff,
     })?;
@@ -317,12 +316,12 @@ fn rlc() -> Result<(), cpu::Error> {
     assert_eq!(cpu.a, 0xff);
     assert_eq!(cpu.condition_codes.cy, 0);
 
-    cpu.execute_instruction(Instruction::RLC)?;
+    cpu.execute_instruction(RLC)?;
 
     assert_eq!(cpu.a, 0xff);
     assert_eq!(cpu.condition_codes.cy, 1);
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 2,
     })?;
@@ -330,7 +329,7 @@ fn rlc() -> Result<(), cpu::Error> {
     assert_eq!(cpu.a, 2);
     assert_eq!(cpu.condition_codes.cy, 1);
 
-    cpu.execute_instruction(Instruction::RLC)?;
+    cpu.execute_instruction(RLC)?;
 
     assert_eq!(cpu.a, 4);
     assert_eq!(cpu.condition_codes.cy, 0);
@@ -343,31 +342,31 @@ fn rlc() -> Result<(), cpu::Error> {
 fn dad() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x1234,
     })?;
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::H,
         value: 0x4321,
     })?;
-    cpu.execute_instruction(Instruction::DAD { register: Reg::B })?;
+    cpu.execute_instruction(DAD { register: Reg::B })?;
 
     assert_eq!(cpu.h, 0x55);
     assert_eq!(cpu.l, 0x55);
 
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x1200,
     })?;
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::H,
         value: 0x0034,
     })?;
     cpu.condition_codes.cy = 1;
-    cpu.execute_instruction(Instruction::DAD { register: Reg::B })?;
+    cpu.execute_instruction(DAD { register: Reg::B })?;
 
     assert_eq!(cpu.h, 0x12);
     assert_eq!(cpu.l, 0x34);
@@ -375,14 +374,14 @@ fn dad() -> Result<(), cpu::Error> {
 
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 1,
     })?;
 
     assert_eq!(cpu.c, 0x01);
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::H,
         value: 0xffff,
     })?;
@@ -390,7 +389,7 @@ fn dad() -> Result<(), cpu::Error> {
     assert_eq!(cpu.h, 0xff);
     assert_eq!(cpu.l, 0xff);
 
-    cpu.execute_instruction(Instruction::DAD { register: Reg::B })?;
+    cpu.execute_instruction(DAD { register: Reg::B })?;
 
     assert_eq!(cpu.h, 0x00);
     assert_eq!(cpu.l, 0x00);
@@ -404,14 +403,14 @@ fn dad() -> Result<(), cpu::Error> {
 fn ldax() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x1234,
     })?;
 
     cpu.memory[0x1234] = 0x11;
 
-    cpu.execute_instruction(Instruction::LDAX { register: Reg::B })?;
+    cpu.execute_instruction(LDAX { register: Reg::B })?;
 
     assert_eq!(cpu.a, 0x11);
 
@@ -423,7 +422,7 @@ fn ldax() -> Result<(), cpu::Error> {
 fn dcx() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x1234,
     })?;
@@ -431,17 +430,17 @@ fn dcx() -> Result<(), cpu::Error> {
     assert_eq!(cpu.b, 0x12);
     assert_eq!(cpu.c, 0x34);
 
-    cpu.execute_instruction(Instruction::DCX { register: Reg::B })?;
+    cpu.execute_instruction(DCX { register: Reg::B })?;
 
     assert_eq!(cpu.b, 0x12);
     assert_eq!(cpu.c, 0x33);
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::D,
         value: 0x0000,
     })?;
 
-    cpu.execute_instruction(Instruction::DCX { register: Reg::D })?;
+    cpu.execute_instruction(DCX { register: Reg::D })?;
 
     assert_eq!(cpu.d, 0xff);
     assert_eq!(cpu.d, 0xff);
@@ -455,15 +454,15 @@ fn dcx() -> Result<(), cpu::Error> {
 fn push() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x1234,
     })?;
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::SP,
         value: 0x0012,
     })?;
-    cpu.execute_instruction(Instruction::PUSH { register: Reg::B })?;
+    cpu.execute_instruction(PUSH { register: Reg::B })?;
 
     assert_eq!(cpu.memory[0x10], 0x34);
     assert_eq!(cpu.memory[0x11], 0x12);
@@ -477,17 +476,17 @@ fn push() -> Result<(), cpu::Error> {
 fn xchg() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::H,
         value: 0x1234,
     })?;
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::D,
         value: 0x1111,
     })?;
 
-    cpu.execute_instruction(Instruction::XCHG)?;
+    cpu.execute_instruction(XCHG)?;
 
     assert_eq!(cpu.d, 0x12);
     assert_eq!(cpu.e, 0x34);
@@ -502,16 +501,16 @@ fn xchg() -> Result<(), cpu::Error> {
 fn pop() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::B,
         value: 0x1234,
     })?;
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::SP,
         value: 0x12,
     })?;
-    cpu.execute_instruction(Instruction::PUSH { register: Reg::B })?;
-    cpu.execute_instruction(Instruction::POP { register: Reg::D })?;
+    cpu.execute_instruction(PUSH { register: Reg::B })?;
+    cpu.execute_instruction(POP { register: Reg::D })?;
 
     assert_eq!(cpu.d, 0x12);
     assert_eq!(cpu.e, 0x34);
@@ -527,7 +526,7 @@ fn out() -> Result<(), cpu::Error> {
 
     let mut cpu = Cpu::new(|v| i = v);
 
-    cpu.execute_instruction(Instruction::OUT { data: 0x12 })?;
+    cpu.execute_instruction(OUT { data: 0x12 })?;
 
     assert_eq!(i, 0x12);
 
@@ -539,17 +538,17 @@ fn out() -> Result<(), cpu::Error> {
 fn add_register() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::B,
         value: 0xFF,
     })?;
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 0x01,
     })?;
 
-    cpu.execute_instruction(Instruction::ADD { register: Reg::B })?;
+    cpu.execute_instruction(ADD { register: Reg::B })?;
 
     assert_eq!(cpu.a, 0);
     assert_eq!(cpu.b, 0xff);
@@ -566,20 +565,20 @@ fn add_register() -> Result<(), cpu::Error> {
 fn add_memory() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::LXI {
+    cpu.execute_instruction(LXI {
         register: Reg::H,
         value: 0x3456,
     })?;
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::M,
         value: 0xff,
     })?;
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 1,
     })?;
-    cpu.execute_instruction(Instruction::ADD { register: Reg::M })?;
+    cpu.execute_instruction(ADD { register: Reg::M })?;
 
     assert_eq!(cpu.a, 0);
     assert_eq!(cpu.memory[0x3456], 0xff);
@@ -596,9 +595,9 @@ fn add_memory() -> Result<(), cpu::Error> {
 fn jz() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::ADD { register: Reg::B })?;
+    cpu.execute_instruction(ADD { register: Reg::B })?;
 
-    let jump_instruction = Instruction::JZ { address: 0x12 };
+    let jump_instruction = JZ { address: 0x12 };
     cpu.execute_instruction(jump_instruction)?;
 
     assert_eq!(cpu.pc, 0x12 - jump_instruction.op_bytes() as u16);
@@ -611,22 +610,22 @@ fn jz() -> Result<(), cpu::Error> {
 fn jc() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::B,
         value: 0xff,
     })?;
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 1,
     })?;
-    cpu.execute_instruction(Instruction::ADD { register: Reg::B })?;
-    let jump_instruction = Instruction::JC { address: 0x82 };
+    cpu.execute_instruction(ADD { register: Reg::B })?;
+    let jump_instruction = JC { address: 0x82 };
     cpu.execute_instruction(jump_instruction)?;
 
     assert_eq!(cpu.pc, 0x82 - jump_instruction.op_bytes() as u16);
 
     cpu.pc = 1;
-    cpu.execute_instruction(Instruction::ADD { register: Reg::B })?;
+    cpu.execute_instruction(ADD { register: Reg::B })?;
     cpu.execute_instruction(jump_instruction)?;
     assert_eq!(cpu.pc, 1);
 
@@ -638,21 +637,21 @@ fn jc() -> Result<(), cpu::Error> {
 fn jnc() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::B,
         value: 0xff,
     })?;
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 1,
     })?;
-    cpu.execute_instruction(Instruction::ADD { register: Reg::B })?;
-    let jump_instruction = Instruction::JNC { address: 0x82 };
+    cpu.execute_instruction(ADD { register: Reg::B })?;
+    let jump_instruction = JNC { address: 0x82 };
     cpu.execute_instruction(jump_instruction)?;
 
     assert_eq!(cpu.pc, 0);
 
-    cpu.execute_instruction(Instruction::ADD { register: Reg::B })?;
+    cpu.execute_instruction(ADD { register: Reg::B })?;
     cpu.execute_instruction(jump_instruction)?;
     assert_eq!(cpu.pc, 0x82 - jump_instruction.op_bytes() as u16);
 
@@ -664,22 +663,22 @@ fn jnc() -> Result<(), cpu::Error> {
 fn rrc() -> Result<(), cpu::Error> {
     let mut cpu = Cpu::new(|_| {});
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 0xC,
     })?;
 
-    cpu.execute_instruction(Instruction::RRC)?;
+    cpu.execute_instruction(RRC)?;
 
     assert_eq!(cpu.a, 6);
     assert_eq!(cpu.condition_codes.cy, 0);
 
-    cpu.execute_instruction(Instruction::MVI {
+    cpu.execute_instruction(MVI {
         register: Reg::A,
         value: 0xD,
     })?;
 
-    cpu.execute_instruction(Instruction::RRC)?;
+    cpu.execute_instruction(RRC)?;
 
     assert_eq!(cpu.a, 0x86);
     assert_eq!(cpu.condition_codes.cy, 1);
@@ -694,14 +693,14 @@ fn rpe() -> Result<(), cpu::Error> {
 
     cpu.condition_codes.p = 1;
 
-    cpu.execute_instruction(Instruction::RPE)?;
+    cpu.execute_instruction(RPE)?;
 
     assert_eq!(cpu.sp, 0x02);
 
     cpu.sp = 0;
     cpu.condition_codes.p = 0;
 
-    cpu.execute_instruction(Instruction::RPE)?;
+    cpu.execute_instruction(RPE)?;
 
     assert_eq!(cpu.sp, 0x00);
 
